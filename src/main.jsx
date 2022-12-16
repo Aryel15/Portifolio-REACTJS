d0c719b53dda57ebf4b3cee14ce1a4541ce0ed81
Aryel Anne Louise de Souza Gabriel <69636549+Aryel15@users.noreply.github.com>
2022-12-16 13:31:34 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Home } from './pages/home/'
-import './index.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
